54bb1bf43eb09298b3c943ad1c412f0546d260f3
unknown <elhadjoumartall10@gmail.com>
2023-06-01 17:28:44 +0200

    Formulaire de html, css et php

---
+++ classes/formulaire.php
@@ -0,0 +1,11 @@
+<?php
+class Formulaire {
+    //On creer des caractéristique private
+    private $type;
+    private $name;
+    // On creer une constante
+    const DEBUT_FORMULAIRE = '<form method="post" action="">';
+    // On creer une nouvelle constante de fin 
+    const FIN_FORMULAIRE = '</form>';
+}
+
